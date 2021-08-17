3ba2a6ee359272b551b15255a441a2d5845dc2f3
GitDogBar <87788268+DogBarCoreMVC@users.noreply.github.com>
2021-08-17 07:24:51 +0700

    Update Context.cs

--- Models/Context.cs
+++ Models/Context.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;//Add Packages
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace SQL_Server_EFCore.Models
 {
-    public class Context : DbContext
+    public class Context : DbContext//Inherit DbContext
     {
         public Context(DbContextOptions<Context>options):base(options)
         {
-
+            //สร้าง Constructor และใช้ความสามารถของ EFCore (DbContextOptions) ในการสร้าง Database
         }
-        DbSet<GroupMenu> groupMenus_tbl { get; set; }
+        DbSet<GroupMenu> groupMenus_tbl { get; set; }//GroupMenu.cs เป็นคลาสที่เราสร้างไว้แต่จะกลายไปเป็น table ใน Database และ groupMenus_tbl คือชื่อ ของ Table
     }
 }
